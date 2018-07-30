7dd1b7a6cc6b33d8348cc9315d0abd459045fdbe
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2018-07-29 21:52:21 -0300

    Adds some methods in TMFile class
    - Adds save method
    - Adds increments method
    - Adds delete method
    - Adds createDirectory method to create if this path is not valid

--- src/Core/TMFile.php
+++ src/Core/TMFile.php
@@ -7,10 +7,60 @@ use TMFiles\Time\{
 
 class TMFile
 {
-    protected $path = null;
+    protected $path    = null;
+    protected $content = null;
 
     public function __construct(string $path) {
         $this->path = $path;
+    }
+
+    /**
+     * Saves the actual content in this file
+     *
+     * @param string $content
+     * @return void
+     */
+    public function save(string $content=null) {
+        if ($content!==null) {
+            $this->content = $content;
+        }
+        $this->createDirectory($this->path);
+        return file_put_contents($this->path, $this->content);
+    }
+
+    /**
+     * Increments content to this file
+     *
+     * @param string $content
+     * @return void
+     */
+    public function increments(string $content) {
+        $this->createDirectory($this->path);
+        $content = $this->content . $content;
+        return file_put_contents($this->path, $content);
+    }
+
+    /**
+     * Deletes this file
+     *
+     * @return void
+     */
+    public function delete() {
+        $response      = unlink($this->path);
+        $this->path    = null;
+        $this->content = null;
+        return $response;
+    }
+
+    /**
+     * Creates an directory
+     *
+     * @param string $path
+     * @param boolean $recursive
+     * @return void
+     */
+    public function createDirectory(string $path, bool $recursive=true) {
+        return mkdir($path, 0777, $recursive);
     }
 
     /**
