8c11bb3e6ccd578b5acb7e2d0ad9d71dc55741be
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2019-05-30 13:19:00 -0300

    Correct consutrction of TimeBase class

--- src/Time/TimeBase.php
+++ src/Time/TimeBase.php
@@ -3,18 +3,30 @@
 namespace TMPHP\Files\Time;
 
 use TMPHPFile;
+use Exception;
 
 class TimeBase
 {
     protected $instance = null;
 
-    public function __construct(string $path=null) {
-        if($path!==null) {
-            $this->instance = new TMPHPFile($path);
+    public function __construct($value=null)
+    {
+        if ($value === null) {
+            throw new Exception("[ERROR] Path is null.", 1);
         }
+        if (is_string($value)) {
+            $this->instance = new TMPHPFile($value);
+            return true;
+        }
+        if (is_a($value, 'TMPHPFile')) {
+            $this->instance = $value;
+            return true;
+        }
+        throw new Exception("[ERROR] Object passed to TimeBase is invalid.", 1);
     }
 
-    public function setInstance($object) {
+    public function setInstance($object)
+    {
         $this->instance = $object;
     }
 
